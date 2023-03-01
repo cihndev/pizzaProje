8ef922d72639950cd6910e2c2a45e1194a984893
cihndev <cihanbasalan@gmail.com>
2023-03-01 18:29:19 +0300

    fifth commit

--- pizzaProje.py
+++ pizzaProje.py
@@ -43,11 +43,12 @@ class Dominos_pizza(Pizza):
 class decorator:
     def __init__(self):
         pass
+    '''
     def get_cost(self):
         return self.component.get_cost() + Pizza.get_cost(self)
     def get_description(self):
         return self.component.get_description() + ' ' + Pizza.get_description(self)
-
+    '''
 class Zeytin(decorator):
     def __init__(self):
         pass
